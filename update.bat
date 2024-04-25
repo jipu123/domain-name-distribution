ce124559e426e3e68b475942ca5d4293385c9241
基蒲 <1109864065@qq.com>
2024-04-25 14:25:08 +0800

    阻止个人文件上传

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 git add *
 git commit -m "first commit"
-git remote add origin git@gitee.com:ji_pu/domain-name-distribution.git
+git remote add origin https://gitee.com/ji_pu/domain-name-distribution.git
 git push -u origin "master"
