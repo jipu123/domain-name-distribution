07c4018ee678b2c74bce1c641df1428ec63a03d7
基蒲 <1109864065@qq.com>
2024-04-26 21:31:57 +0800

    写完邀请码管理

--- app/controller/Admin.php
+++ app/controller/Admin.php
@@ -43,25 +43,25 @@ class Admin extends BaseController
     {
         if (!$this->authenticate()) return redirect("/admin/login");
         View::assign("records", Db::table("records")->select()->count());
-        View::assign("domain",DB::table("domain")->select()->count());
-        View::assign("user",DB::table("user")->select()->count());
-        View::assign("tickets",DB::table("tickets")->field('identity')->distinct(true)->count());
+        View::assign("domain", DB::table("domain")->select()->count());
+        View::assign("user", DB::table("user")->select()->count());
+        View::assign("tickets", DB::table("tickets")->field('identity')->distinct(true)->count());
         //获取回复的工单数量
-        $time = strtotime(date('Y-m-d',strtotime(date('Y-m-d',time()))-(86400*6)));//获取七天前的时间戳
+        $time = strtotime(date('Y-m-d', strtotime(date('Y-m-d', time())) - (86400 * 6))); //获取七天前的时间戳
         $count = [];
-        for ($i=0; $i < 7; $i++) { 
-            array_push($count,Db::table("tickets")->whereDay('up_time',date('Y-m-d',$time))->count());
-            $time+=86400;
+        for ($i = 0; $i < 7; $i++) {
+            array_push($count, Db::table("tickets")->whereDay('up_time', date('Y-m-d', $time))->count());
+            $time += 86400;
         }
-        View::assign("tickets_count","[".implode(",",$count)."]");
+        View::assign("tickets_count", "[" . implode(",", $count) . "]");
         //获取审核量
-        $time = strtotime(date('Y-m-d',strtotime(date('Y-m-d',time()))-(86400*6)));//获取七天前的时间戳
+        $time = strtotime(date('Y-m-d', strtotime(date('Y-m-d', time())) - (86400 * 6))); //获取七天前的时间戳
         $count = [];
-        for ($i=0; $i < 7; $i++) { 
-            array_push($count,Db::table("censor")->whereDay('create_time',date('Y-m-d',$time))->count());
-            $time+=86400;
+        for ($i = 0; $i < 7; $i++) {
+            array_push($count, Db::table("censor")->whereDay('create_time', date('Y-m-d', $time))->count());
+            $time += 86400;
         }
-        View::assign("censor_count","[".implode(",",$count)."]");
+        View::assign("censor_count", "[" . implode(",", $count) . "]");
         return View::fetch();
     }
 
@@ -107,22 +107,94 @@ class Admin extends BaseController
                 "msg" => "鉴权失败"
             ]);
         } //鉴权错误
-        switch(Request::param("tag")){
+        switch (Request::param("tag")) {
             case "update_password":
                 $db = Db::table("user")->where("ukey", cookie("ukey"))->find();
-                if(Db::table("user")->where("id",$db["id"])->update(["password"=>md5(Request::param("password"))])){
+                if (Db::table("user")->where("id", $db["id"])->update(["password" => md5(Request::param("password"))])) {
                     return json([
-                        "code"=>200,
-                        "msg"=>"修改成功"
+                        "code" => 200,
+                        "msg" => "修改成功"
                     ]);
+                } else {
+                    return json([
+                        "code" => 0,
+                        "msg" => "修改失败"
+                    ]);
+                }
+                break;
+            case "invite":
+                $db = Db::table("invite")->order("id", "desc")->select()->toarray();
+                if(Request::param("act")=="list"){
+                    return json($db);
+                }elseif(Request::param("act")=="xg"){
+                    $db = Db::table("invite")->where("id",Request::param("id"))->update(["is_lock"=>Request::param("is_lock")]);
+                    if($db){
+                        return json([
+                            "code" => 200,
+                            "msg" => "修改成功"
+                        ]);
+                    }else{
+                        return json([
+                            "code" => 0,
+                            "msg" => "修改失败"
+                        ]);
+                    }
+                }elseif(Request::param("act")=="add"){
+                    $invite = trim(Request::param("invite"));
+                    $current = Request::param("current");
+                    $domain = Request::param("domain");
+                    $record = Request::param("record");
+                    $time = Request::param("time");
+                    if(empty($invite)){
+                        return json([
+                            "code" => 0,
+                            "msg" => "邀请码不能为空"
+                        ]);
+                    }
+                    if(!preg_match("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $time)) {
+                        return json([
+                            "code" => 0,
+                            "msg" => "时间格式必须为YYYY-mm-dd"
+                        ]);
+                    }
+                    if(!is_numeric($current) || !is_numeric($domain) || !is_numeric($record)){
+                        return json([
+                            "code" => 0,
+                            "msg" => "必须为数字"
+                        ]);
+                    }
+                    if(Db::table("invite")->where("value",$invite)->find()){
+                        return json([
+                            "code" => 0,
+                            "msg" => "邀请码已存在"
+                        ]);
+                    }
+                    if(Db::table("invite")->insert([
+                        "value"=>$invite,
+                        "max"=>$current,
+                        "domain_num"=>$domain,
+                        "record_num"=>$record,
+                        "over_time"=>$time
+                    ])){
+                        return json([
+                            "code" => 200,
+                            "msg" => "添加成功"
+                        ]);
+                    }else{
+                        return json([
+                            "code" => 0,
+                            "msg" => "添加失败"
+                        ]);
+                    }
                 }else{
                     return json([
-                        "code"=>0,
-                        "msg"=>"修改失败"
+                        "code" => 0,
+                        "msg" => "未知操作"
                     ]);
                 }
                 break;
         }
+        print_r(Request::param());
     }
 
     public function login()
